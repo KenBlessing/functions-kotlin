51c981552d02425b5cee94f3090746eeba5a8984
Fredrick Amima <39988319+KenBlessing@users.noreply.github.com>
2023-02-03 20:43:53 +0300

    Add files via upload

---
+++ HelloWorld.kt
@@ -0,0 +1,14 @@
+fun main(){
+    
+   println(newfunc("kenya"))
+    
+}
+   
+   fun newfunc(name:String):String{
+   val age = 20
+   val name = "my name $name"
+   
+   return "$name\n$age"
+   
+   /*print("My name is: ${Name}")*/
+   }
